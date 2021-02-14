790d061e2e9c5ec64d9042812bbb787d61e14784
Ebrima Tunkara <ebrimatunkara@gmail.com>
2021-02-14 12:54:31 -0500

    exclude default spring batch data source

--- src/main/java/dev/tunks/taxitrips/batch/BatchDataProcessorApplication.java
+++ src/main/java/dev/tunks/taxitrips/batch/BatchDataProcessorApplication.java
@@ -2,8 +2,9 @@ package dev.tunks.taxitrips.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class BatchDataProcessorApplication {
 
 	public static void main(String[] args) {
