8a0b8b02c3cd365d3feadf5e6f65eec155af87f4
Ebrima Tunkara <ebrimatunkara@gmail.com>
2021-02-14 12:14:02 -0500

    added maven dependencies and first commit

---
+++ src/main/java/dev/tunks/taxitrips/mstlcdataprocessor/MsTlcDataProcessorApplication.java
@@ -0,0 +1,13 @@
+package dev.tunks.taxitrips.mstlcdataprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTlcDataProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTlcDataProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tunks/taxitrips/mstlcdataprocessor/MsTlcDataProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tunks.taxitrips.mstlcdataprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTlcDataProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
